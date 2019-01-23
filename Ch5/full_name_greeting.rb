028658ae335fd08c696cfe27eb92f431fada0991
makk <mkahack@hawk.iit.edu>
2019-01-23 15:18:41 -0600

    A program that asks for first, middle, and last name and greets user

---
+++ Ch5/full_name_greeting.rb
@@ -0,0 +1,8 @@
+puts 'What\'s your first name?'
+firstname = gets.chomp
+puts 'What\'s your middle name?'
+middlename = gets.chomp
+puts 'What\'s your last name?'
+lastname = gets.chomp
+
+puts 'Hi ' + firstname + ' ' + middlename + ' ' + lastname + ', nice to meet you!'
