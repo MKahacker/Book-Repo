2dfc13baf712977d0a8536a50f2527ce2d387aa5
makk <mkahack@hawk.iit.edu>
2019-02-03 12:14:43 -0600

    Dragon pet game

---
+++ Ch13/dragon.rb
@@ -0,0 +1,121 @@
+class Dragon
+
+  def initialize name
+    @name = name
+    @asleep = false
+    @stuff_in_belly = 10
+    @stuff_in_intestine = 0
+
+    puts "#{@name} is born."
+  end
+
+  def feed
+    puts "You feed #{@name}"
+    @stuff_in_belly = 10
+    passage_of_time
+  end
+
+  def walk
+    puts "You walk #{@name}"
+    @stuff_in_intestine = 0
+    passage_of_time
+  end
+
+  def put_to_bed
+    puts "You put #{@name} to bed."
+    @asleep = true
+    3.times do
+      if @asleep
+        passage_of_time
+      end
+      if @asleep
+        puts "#{@name} snores, filling the room with smoke."
+      end
+    end
+    if @asleep
+      @asleep = false
+      puts "#{@name} wakes up slowly."
+    end
+  end
+
+  def toss
+    puts "You toss #{@name} up into the air."
+    puts "He giggles, which singes your eyebrows"
+    passage_of_time
+  end
+
+  def rock
+    puts "You rock #{@name} gently."
+    @asleep = true
+    puts "He briefly dozes off..."
+    passage_of_time
+    if @asleep
+      @asleep = false
+      puts "...but wakes when you stop."
+    end
+  end
+
+  private
+  def hungry?
+      @stuff_in_belly <= 2
+  end
+
+  def poopy?
+    @stuff_in_intestine >= 8
+  end
+
+  def passage_of_time
+    if @stuff_in_belly > 0
+      @stuff_in_belly = @stuff_in_belly - 1
+      @stuff_in_intestine = @stuff_in_intestine + 1
+    else
+      if @asleep
+        @asleep = false
+        puts "He wakes up suddenly!"
+      end
+      puts "#{@name} is starving! In desperation, he ate YOU!"
+      exit
+    end
+    if @stuff_in_intestine >= 10
+      @stuff_in_intestine = 0
+      puts "Whoops! #{@name} had an accident..."
+    end
+    if hungry?
+      if @asleep
+        @asleep = false
+        puts 'He wakes up suddenly!'
+      end
+      puts "#{@name}'s stomach grumbles..."
+    end
+    if poopy?
+      if @asleep
+        @asleep = false
+        puts "He wakes up suddenly!"
+      end
+      puts "#{@name} does the potty dance..."
+    end
+  end
+end
+
+puts "Welcome to the dragon game!"
+puts "Name your dragon!"
+name = gets.chomp
+pet = Dragon.new name
+while true
+  puts "enter f to feed dragon, enter w to walk dragon, enter s to put dragon to bed."
+  puts "enter t to toss dragon, and enter r to rock dragon."
+  move = gets.chomp
+  if move == 'f'
+    pet.feed
+  elsif move == 'w'
+    pet.walk
+  elsif move == 's'
+    pet.put_to_bed
+  elsif move == 't'
+    pet.toss
+  elsif move == 'r'
+    pet.rock
+  else
+    puts "Not a valid move please try again."
+  end
+end
