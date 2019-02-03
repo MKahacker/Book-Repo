df333a0a6c5f216d71ab2b72387aa9f5cc1e7295
makk <mkahack@hawk.iit.edu>
2019-02-02 20:28:42 -0600

    Excersize from chapter 11

---
+++ Ch11/Double-Quote.rb
@@ -0,0 +1,11 @@
+buffy_quote_1 = '\'Kiss Rocks\'?
+                  Why would anyone want to kiss...
+                  Oh, wait. I get it.'
+
+buffy_quote_2 = "'Kiss Rocks'?\n" +
+                "Why would anyone want to kiss...\n" +
+                "Oh, wait. I get it."
+puts buffy_quote_1
+puts
+puts (buffy_quote_1 == buffy_quote_2)
+puts "3...\n2...\n1...\nHAPPY NEW YEAR!"
---
+++ Ch11/interpolation.rb
@@ -0,0 +1,6 @@
+name = 'Luke'
+zip = 90210
+
+puts "Name = #{name}, Zipcode = #{zip}"
+
+puts "#{2 * 10**4 + 1} Leagues Under the Sea, THE REVENGE!"
